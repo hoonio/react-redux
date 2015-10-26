b48bf5993ebbdc6b4b84219c4db3cd008db656b1
Hoonio <hoonio@hoonio.com>
2015-10-26 22:40:10 +0000

    accessing child properties

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -5,43 +5,22 @@ window.React = React;
 const mountNode = document.getElementById('app');
 
 var App = React.createClass({
-  getInitialState:function(){
-    return {
-      red: 0,
-      green: 0,
-      blue: 0
-    }
-  },
-  update:function(){
-    this.setState({
-      red: this.refs.red.refs.inp.getDOMNode().value,
-      green: this.refs.green.refs.inp.getDOMNode().value,
-      blue: this.refs.blue.refs.inp.getDOMNode().value
-    })
-  },
-  render: function(){
-    return (
-      <div>
-        <Slider ref="red" update={this.update} />
-        <label>{this.state.red}</label>
-        <Slider ref="green" update={this.update} />
-        <label>{this.state.green}</label>
-        <Slider ref="blue" update={this.update} />
-        <label>{this.state.blue}</label>
-      </div>
-    );
+  render:function(){
+    return <Button>I <Heart /> React</Button>
   }
 });
 
-var Slider = React.createClass({
-  render: function(){
-    return (
-      <div>
-        <input ref="inp" type="range" min="0" max="255" onChange={this.props.update} />
-      </div>
-    );
+var Button = React.createClass({
+  render:function(){
+    return <button>{this.props.children}</button>
   }
-})
+});
+
+var Heart = React.createClass({
+  render:function(){
+    return <span className="glyphicon glyphicon-heart"></span>
+  }
+});
 
 React.render(<App txt="this is the props value" />, mountNode);
 // React.render(<Home/>, mountNode);
