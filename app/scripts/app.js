90968c41bda34bed9b6f67026bb14f174ec0e2dd
Hoonio <hoonio@hoonio.com>
2015-10-26 22:35:51 +0000

    access components refs

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -7,22 +7,41 @@ const mountNode = document.getElementById('app');
 var App = React.createClass({
   getInitialState:function(){
     return {
-      txt: 'the state txt'
+      red: 0,
+      green: 0,
+      blue: 0
     }
   },
-  update:function(e){
-    this.setState({txt: e.target.value})
+  update:function(){
+    this.setState({
+      red: this.refs.red.refs.inp.getDOMNode().value,
+      green: this.refs.green.refs.inp.getDOMNode().value,
+      blue: this.refs.blue.refs.inp.getDOMNode().value
+    })
   },
   render: function(){
-    var txt = this.state.txt;
     return (
       <div>
-        <input type="text" onChange={this.update} />
-        <h1>{txt}</h1>
+        <Slider ref="red" update={this.update} />
+        <label>{this.state.red}</label>
+        <Slider ref="green" update={this.update} />
+        <label>{this.state.green}</label>
+        <Slider ref="blue" update={this.update} />
+        <label>{this.state.blue}</label>
       </div>
     );
   }
 });
+
+var Slider = React.createClass({
+  render: function(){
+    return (
+      <div>
+        <input ref="inp" type="range" min="0" max="255" onChange={this.props.update} />
+      </div>
+    );
+  }
+})
 
 React.render(<App txt="this is the props value" />, mountNode);
 // React.render(<Home/>, mountNode);
