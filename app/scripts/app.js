1a3a3078fbec67e2c089501af7cf0f178458d186
Hoonio <hoonio@hoonio.com>
2015-11-04 23:35:51 +0000

    dynamically generated components

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,72 +4,37 @@ import Home from './components/home';
 window.React = React;
 const mountNode = document.getElementById('app');
 
-var App = React.createClass({
+var PersonTable = React.createClass({
   getInitialState:function(){
     return {
-      red: 0
+      data: [
+        {id: 1, fname: "Simon", lname: "Bailey"},
+        {id: 2, fname: "Thomas", lname: "Bailey"},
+        {id: 3, fname: "Will", lname: "Bailey"},
+        {id: 4, fname: "Kent", lname: "Bailey"},
+      ]
     }
   },
-  update:function(){
-    this.setState({
-      red: this.refs.red.refs.inp.getDOMNode().value
+
+  render:function(){
+    var rows = this.state.data.map(function(person, i){
+      return <PersonRow data={person} key={i} />
     });
-  },
-  render: function(){
-    return (
-      <div>
-        <NumInput
-          ref="red"
-          min={0}
-          max={255}
-          step={1}
-          val={+this.state.red}
-          type="number"
-          label="Red"
-          update={this.update} />
-      </div>
-    );
+    return <table>{rows}</table>
   }
 });
 
-var NumInput = React.createClass({
-  propTypes: {
-    min: React.PropTypes.number,
-    max: React.PropTypes.number,
-    step: React.PropTypes.number,
-    val: React.PropTypes.number,
-    label: React.PropTypes.string,
-    update: React.PropTypes.func.isRequired,
-    type: React.PropTypes.oneOf(['number', 'range'])
-  },
-  getDefaultProps:function(){
-    return {
-      min: 0,
-      max: 0,
-      step: 1,
-      val: 0,
-      label: '',
-      type: 'range'
-    }
-  },
-  render: function(){
-    var label = this.props.label !== '' ?
-      <label>{this.props.label} {this.props.val}</label> : ''
+var PersonRow = React.createClass({
+  render:function(){
     return (
-      <div>
-        <input
-          ref="inp"
-          type={this.props.type}
-          min={this.props.min}
-          max={this.props.max}
-          step={this.props.step}
-          defaultValue={this.props.val}
-          onChange={this.props.update} />
-          {label}
-      </div>
-    );
+      <tr>
+        <td>{this.props.data.id}</td>
+        <td>{this.props.data.fname}</td>
+        <td>{this.props.data.lname}</td>
+      </tr>
+    )
   }
-})
+});
 
-React.render(<App />, mountNode);
+React.render(<PersonTable />, mountNode);
 // React.render(<Home/>, mountNode);
