1ba2263289691e848b229cf9d0ecac8e2f2d8f20
Hoonio <hoonio@hoonio.com>
2015-11-27 16:56:20 +0000

    async get data from google sheets

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -11,12 +11,14 @@ var App = React.createClass({
     }
   },
   componentWillMount:function(){
+    // var Alert = ReactBootstrap.Alert;
     reqwest({
-      url:'http://filltext.com/?rows=10&val={randomNumber}',
+      url:'https://spreadsheets.google.com/feeds/list/1LNTNp3n_DYYq_dDLf7YdZyJWjI0soMn3MjYPeVLFSfk/1/public/values?alt=json',
       type: 'jsonp',
       success:function(resp){
-        this.setState({data:resp});
-        this.renderChart(this.state.data);
+        this.setState({data:resp.feed.entry});
+        console.log(this.state.data);
+        // this.renderChart(this.state.data);
       }.bind(this)
     })
   },
@@ -31,7 +33,30 @@ var App = React.createClass({
       });
   },
   render: function(){
-    return <div id="chart"></div>
+    var works = [];
+    for (var i=0; i<this.state.data.length; i++) {
+      var workItem = this.state.data[i];
+      works.push(<div class="col-sm-6 col-md-4">
+              <div class="thumbnail">
+                <img src={workItem.gsx$image.$t} alt="" />
+                <div class="caption">
+                  <h3>{workItem.gsx$title.$t}</h3>
+                  <p>{workItem.gsx$description.$t}</p>
+                  <p>
+                    {workItem.gsx$stacks.$t}
+                    <a href={workItem.gsx$link.$t} class="btn btn-primary" role="button">Link</a>
+                    <a href="#" class="btn btn-default" role="button">Source</a>
+                  </p>
+                </div>
+              </div>
+            </div>);
+    }
+    return <div class="row">{works}</div>
+    // <div>
+    //   <p>{this.state.data[0]}</p>
+    //   <img src={this.state.data[0].gsx$image.$t} alt="" />
+    // </div>
+
   }
 });
 
