3199d35d4e6a193f38df6d6ed45e9b4972bba5a1
Hoonio <hoonio@hoonio.com>
2016-02-27 02:24:44 +0000

    add main frame for router in the future

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,14 +4,13 @@ import Router from './routes';
 import Home from './components/home';
 import Nav from './components/navbar';
 import Portfolio from './components/Portfolio';
+import Main from './components/Main';
 
 window.React = React;
 const mountNode = document.getElementById('app');
-const mountNav = document.getElementById('nav');
 
 var App = React.createClass({
-  mixins:[Addons.LinkedStateMixin],
-  getInitialState:function(){
+  getInitialState(){
     return {
       name:'',
       email:'',
@@ -24,12 +23,15 @@ var App = React.createClass({
   //     email:this.refs.email.getDOMNode().value
   //   })
   // },
-  render:function(){
+  render(){
     return (
-      <div></div>
+      <div className="container">
+        <Nav />
+        <Main />
+        <Portfolio />
+      </div>
     )
   }
 });
 
-React.render(<Portfolio />, mountNode);
-React.render(<Nav />, mountNav);
+React.render(<App />, mountNode);
--- app/scripts/routes.js
+++ app/scripts/routes.js
@@ -1,4 +1,8 @@
-// import React from 'react';
+import React from 'react';
+import { Router, Route, IndexRoute } from 'react-router';
+import Main from './components/main';
+import Home from './components/home';
+import Portfolio from './components/Portfolio';
 // import Router from 'react-router/es6/Router';
 //
 // export default class extends React.Component {
@@ -8,3 +12,8 @@
 //   // const router = new Router()
 //
 // }
+
+// module.exports = (
+//   <Route path="/" component={Main}>
+//     Route path=""
+// )
