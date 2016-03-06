06405449bffff3169ecc2479f1a4a8422b5a61d1
Hoonio <hoonio@hoonio.com>
2016-03-06 13:24:06 +0000

    masonry-like columns from Bootstrap4

--- app/scripts/components/portfolio.js
+++ app/scripts/components/portfolio.js
@@ -20,9 +20,14 @@ export default class extends React.Component {
       url:'https://spreadsheets.google.com/feeds/list/1LNTNp3n_DYYq_dDLf7YdZyJWjI0soMn3MjYPeVLFSfk/1/public/values?alt=json-in-script',
       type: 'jsonp',
       success:(resp) => {
+        this.organizeData(resp.feed.entry);
         this.setState({items: resp.feed.entry});
       }.bind(this)
     });
+  }
+
+  organizeData(source) {
+    console.log(source);
   }
 
   renderChart(dataset) {
@@ -38,13 +43,15 @@ export default class extends React.Component {
 
   render() {
     return (
-      <div className="portfolio row">
+      <div id="portfolio">
         <div id="chart"></div>
         <h1>Portfolio</h1>
         <p>Here are all the works</p>
-        {this.state.items.map( item => {
-          return <WorkItem item={item} />
-        })}
+          <div className="card-columns">
+            {this.state.items.map( item => {
+              return <WorkItem item={item} />
+            })}
+          </div>
       </div>
     );
   }
--- app/scripts/components/workitem.js
+++ app/scripts/components/workitem.js
@@ -6,15 +6,16 @@ export default (props) => {
     color: '#000'
   }
   return (
-    <div className="col-sm-4" style={itemStyle}>
-      <div className="card">
-        <img className="card-img-top img-fluid" src={props.item.gsx$image.$t} alt={props.item.gsx$imagealt.$t} />
-        <div className="card-block">
-          <h4 className="card-title">{props.item.gsx$title.$t}</h4>
-          <p className="card-text">{props.item.gsx$description.$t}</p>
-          <p className="card-text">{props.item.gsx$stacks.$t}</p>
-          <a href="{props.item.gsx$link.$t}" className="btn btn- primary">Button</a>
-        </div>
+    <div className="card">
+      <div className="card-block">
+        <h4 className="card-title">{props.item.gsx$title.$t}</h4>
+        <h6 className="card-subtitle text-muted">{props.item.gsx$client.$t}</h6>
+      </div>
+      <img className="card-img-top img-fluid" src={props.item.gsx$image.$t} alt={props.item.gsx$imagealt.$t} />
+      <div className="card-block">
+        <p className="card-text">{props.item.gsx$stacks.$t}</p>
+        <a href={props.item.gsx$link.$t} className="btn btn-primary card-link">Button</a>
+        <a href={props.item.gsx$sourcecode.$t} className={props.item.gsx$sourcecode.$t ? 'card-link' : 'invisible'} >Source</a>
       </div>
     </div>
   )
