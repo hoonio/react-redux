b6bec91fd9d8d646cf782efb26083a6467b6f737
Hoonio <hoonio@hoonio.com>
2016-03-06 01:14:57 +0000

    insert image files into portfolio list

--- app/scripts/components/workitem.js
+++ app/scripts/components/workitem.js
@@ -2,17 +2,18 @@ import React from 'react'
 
 export default (props) => {
   let itemStyle = {
-    border: '1px solid #ccc',
-    color: '#F00'
+    border: '0px solid #ccc',
+    color: '#000'
   }
   return (
     <div className="col-sm-4" style={itemStyle}>
       <div className="card">
-        <img className="card-img-top" data-src="{props.item.gsx$image.$t}" alt="Card image cap" />
+        <img className="card-img-top img-fluid" src={props.item.gsx$image.$t} alt={props.item.gsx$imagealt.$t} />
         <div className="card-block">
           <h4 className="card-title">{props.item.gsx$title.$t}</h4>
-          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the </p>
-          <a href="#" className="btn btn-primary">Button</a>
+          <p className="card-text">{props.item.gsx$description.$t}</p>
+          <p className="card-text">{props.item.gsx$stacks.$t}</p>
+          <a href="{props.item.gsx$link.$t}" className="btn btn- primary">Button</a>
         </div>
       </div>
     </div>
