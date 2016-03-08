d06619cf3e011896a5692dc6d28a7aff9107419e
Hoonio <hoonio@hoonio.com>
2016-03-08 03:06:39 +0000

    simplify component definitions to es6 syntax

--- app/scripts/components/navlink.js
+++ app/scripts/components/navlink.js
@@ -1,10 +1,8 @@
 import React from 'react'
 import {Link} from 'react-router'
 
-export default (props) => {
-  return(
-    <li className="nav-item">
-      <Link {...props} className="nav-link" activeClassName="active" />
-    </li>
-  )
-}
+export default (props) => (
+  <li className="nav-item">
+    <Link {...props} className="nav-link" activeClassName="active" />
+  </li>
+);
--- app/scripts/components/profile.js
+++ app/scripts/components/profile.js
@@ -3,15 +3,23 @@ import React from 'react'
 export default class extends React.Component{
   constructor(props) {
     super(props);
-    reqwest({
-      url:'https://docs.google.com/document/d/1uQQfy_U0ekb0lo6SjKb6-j1MLTFxwVNR2Ep4H9DcPDI/pub',
-      type: 'jsonp',
-      contentType: 'text/html',
-      success:(resp) => {
-        console.log(resp)
-        this.organizeData(resp);
-      }.bind(this)
-    });
+    // reqwest({
+    //   url:'https://docs.google.com/document/d/1uQQfy_U0ekb0lo6SjKb6-j1MLTFxwVNR2Ep4H9DcPDI/pub',
+    //   type: 'jsonp',
+    //   contentType: 'text/html',
+    //   success:(resp) => {
+    //     console.log(resp)
+    //     this.organizeData(resp);
+    //   }.bind(this)
+    // });
+
+  }
+
+  hackIframe() {
+    const cv = document.querySelectorAll('iframe');
+    console.log(cv);
+    const cvName = document.getElementById('h.tm6ltssgrugr');
+    console.log(cvName);
   }
 
   organizeData(source) {
@@ -20,8 +28,17 @@ export default class extends React.Component{
 
   render(){
     return (
-      <div>My rendered CV or resume goes here</div>
+      <div id="profile" className="row">
+        My rendered CV or resume goes here
+        <div className="col-sm-12">
+          <iframe src="https://docs.google.com/document/d/1uQQfy_U0ekb0lo6SjKb6-j1MLTFxwVNR2Ep4H9DcPDI/pub?embedded=true" width="100%"></iframe>
+        </div>
+      </div>
     );
+  }
+
+  componentDidMount(){
+    this.hackIframe();
   }
 
 }
