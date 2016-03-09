10143df0eb5e28db5015913ac3c4c81a5a1f7e1d
Hoonio <hoonio@hoonio.com>
2016-03-09 01:19:35 +0000

    turn stacks list into array, map each as buttons

--- app/scripts/components/workitem.js
+++ app/scripts/components/workitem.js
@@ -6,11 +6,20 @@ export default ({item}) => (
       <h4 className="card-title">{item.gsx$title.$t}</h4>
       <h6 className="card-subtitle text-muted">{item.gsx$period.$t} {item.gsx$client.$t}</h6>
     </div>
-    <img className="card-img-top img-fluid" src={item.gsx$image.$t} alt={item.gsx$imagealt.$t} />
+    <img className={item.gsx$image.$t ? 'card-img-top img-fluid' : 'invisible'} src={item.gsx$image.$t} alt={item.gsx$imagealt.$t} />
     <div className="card-block">
-      <p className="card-text">{item.gsx$stacks.$t}</p>
-      <a href={item.gsx$link.$t} className="btn btn-primary card-link">Button</a>
-      <a href={item.gsx$sourcecode.$t} className={item.gsx$sourcecode.$t ? 'card-link' : 'invisible'} >Source</a>
+      <p className={item.gsx$description.$t ? 'card-text' : 'invisible'}>{item.gsx$description.$t}</p>
+      <p className={item.gsx$stacks.$t ? 'card-text' : 'invisible'}>{listStacks(item.gsx$stacks.$t).map((stack) => (
+        <button type="button" className="btn-sm btn-secondary">{stack}</button>
+      ))}</p>
+      <a href={item.gsx$link.$t} className="btn btn-primary card-link">Demo</a>
+      <a href={item.gsx$sourcecode.$t} className={item.gsx$sourcecode.$t ? 'card-link' : 'invisible'} >Code</a>
     </div>
   </div>
 );
+
+const listStacks = (stringText) => {
+  let arr = stringText.split(", ")
+  console.log(arr);
+  return arr
+}
