ff63856ff5a3ff8bef145cdba0a4e74380981b16
Hoonio <hoonio@hoonio.com>
2016-03-09 01:26:04 +0000

    dangerously reformat description string into markup

--- app/scripts/components/workitem.js
+++ app/scripts/components/workitem.js
@@ -8,7 +8,7 @@ export default ({item}) => (
     </div>
     <img className={item.gsx$image.$t ? 'card-img-top img-fluid' : 'invisible'} src={item.gsx$image.$t} alt={item.gsx$imagealt.$t} />
     <div className="card-block">
-      <p className={item.gsx$description.$t ? 'card-text' : 'invisible'}>{item.gsx$description.$t}</p>
+      <p className={item.gsx$description.$t ? 'card-text' : 'invisible'} dangerouslySetInnerHTML={createMarkup(item.gsx$description.$t)} />
       <p className={item.gsx$stacks.$t ? 'card-text' : 'invisible'}>{listStacks(item.gsx$stacks.$t).map((stack) => (
         <button type="button" className="btn-sm btn-secondary">{stack}</button>
       ))}</p>
@@ -22,4 +22,8 @@ const listStacks = (stringText) => {
   let arr = stringText.split(", ")
   console.log(arr);
   return arr
+}
+
+const createMarkup = (htmlString) => {
+  return {__html : htmlString}
 }
