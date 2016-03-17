8f9f29f01389694943be661093d80a97dfb4875f
Hoonio <hoonio@hoonio.com>
2016-03-17 21:46:51 +0000

    add target to images in portfolio

--- app/scripts/components/workitem.js
+++ app/scripts/components/workitem.js
@@ -6,13 +6,15 @@ export default ({item, index}) => (
       <h4 className="card-title">{item.gsx$title.$t}</h4>
       <h6 className="card-subtitle text-muted">{item.gsx$period.$t} {item.gsx$client.$t}</h6>
     </div>
-    <img className={item.gsx$image.$t ? 'card-img-top img-fluid' : 'invisible'} src={item.gsx$image.$t} alt={item.gsx$imagealt.$t} />
+    <a href={item.gsx$link.$t ? item.gsx$link.$t : '#'}>
+      <img className={item.gsx$image.$t ? 'card-img-top img-fluid' : 'invisible'} src={item.gsx$image.$t} alt={item.gsx$imagealt.$t} />
+    </a>
     <div className="card-block">
       <p className={item.gsx$description.$t ? 'card-text' : 'invisible'} dangerouslySetInnerHTML={createMarkup(item.gsx$description.$t)} />
       <p className={item.gsx$stacks.$t ? 'card-text' : 'invisible'}>{listStacks(item.gsx$stacks.$t).map((stack, index) => (
         <button type="button" className="btn-sm btn-secondary" key={index}>{stack}</button>
       ))}</p>
-      <a href={item.gsx$link.$t} className="btn btn-primary card-link">Demo</a>
+      <a href={item.gsx$link.$t} className={item.gsx$link.$t ? 'btn btn-primary card-link' : 'invisible'}>Demo</a>
       <a href={item.gsx$sourcecode.$t} className={item.gsx$sourcecode.$t ? 'card-link' : 'invisible'} >Code</a>
     </div>
   </div>
