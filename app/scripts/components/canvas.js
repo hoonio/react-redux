75b33a6e41adcb6b23b940d8537f4b914217d5be
Hoonio <hoonio@hoonio.com>
2016-03-23 01:07:43 +0000

    D3: svg

--- app/scripts/components/canvas.js
+++ app/scripts/components/canvas.js
@@ -6,25 +6,53 @@ export default class extends React.Component {
     this.state = {
       data: []
     };
-    reqwest({
-      url:'//filltext.com/?rows=10&val={randomNumber}',
-      type: 'jsonp',
-      success:function(resp){
-        this.setState({data:resp});
-        this.renderChart(this.state.data);
-      }.bind(this)
-    });
+    // reqwest({
+    //   url:'//filltext.com/?rows=10&val={randomNumber}',
+    //   type: 'jsonp',
+    //   success:function(resp){
+    //     this.setState({data:resp});
+    //     this.renderChart(this.state.data);
+    //   }.bind(this)
+    // });
   }
 
-  renderChart(dataset) {
-    d3.select('#chart').selectAll('div')
+  renderChart(remoteData) {
+
+    console.log('Render D3')
+    let dataset = [5,10,15,20,25]
+
+    // d3.select('#chart').selectAll('div')
+    //   .data(dataset)
+    //   .enter()
+    //   .append('div')
+    //   .attr('class', 'bar')
+    //   .style('height', function(d){
+    //     return d.val*8+'px';
+    // });
+    var svg = d3.select('#chart').append('svg')
+      .attr('width', 400)
+      .attr('height', 300);
+    console.log(svg)
+
+    svg.selectAll('div')
       .data(dataset)
       .enter()
-      .append('div')
+      .append('rect')
       .attr('class', 'bar')
+      .attr('x', function(d,i) {
+        return i*20+2;
+      })
+        .attr('y', function(d) {
+          return 300 - d*8
+        })
+      .attr('width', 20)
       .style('height', function(d){
-        return d.val*5+'px';
+        return d*8;
      });
+  }
+
+  componentDidMount() {
+    this.renderChart(this.state.data)
   }
 
   render() {
