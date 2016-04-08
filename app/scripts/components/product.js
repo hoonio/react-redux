b1eb8dc31517c725e59826ded68be23d1a6775b7
Hoonio <hoonio@hoonio.com>
2016-04-08 16:58:29 +0100

    tidy up product list in /home

--- app/scripts/components/product.js
+++ app/scripts/components/product.js
@@ -1,25 +1,28 @@
 import React from 'react';
 
 export default (props) => (
-  <div className={(props.prod.imageOn=='right') ? 'content-section-a' : 'content-section-b' }>
+  <div className={(props.prod.imageOn=='right') ?
+    'content-section-a' : 'content-section-b' }>
     <div className="container-fluid">
       <div className="row">
-        <div className={(props.prod.imageOn=='right') ? "col-lg-5 col-lg-offset-1 col-sm-6 col-sm-push-6" : "col-lg-5 col-sm-6" }>
+        <div className={(props.prod.imageOn=='right') ?
+         'col-lg-5 col-lg-offset-1 col-sm-6 col-sm-push-6' :
+         'col-lg-5 col-sm-6' }>
           <div className="row">
-            <a href={props.prod.target}><img className="img-responsive img-fluid pull-sm-right" src={props.prod.img} alt="" /></a>
+            <a href={props.prod.target}>
+              <img className="img-responsive img-fluid pull-sm-right" src={props.prod.img}/>
+            </a>
           </div>
         </div>
-        <div className={(props.prod.imageOn=='right') ? "col-lg-5 col-lg-offset-1 col-sm-6 col-sm-pull-6" : "col-lg-5 col-lg-offset-1 col-sm-6" }>
-          <hr className="section-heading-spacer" />
-          <div className="clearfix"></div>
-          <a href={props.prod.target}><h2 className="section-heading">{props.prod.heading}</h2></a>
-          <p className="lead" dangerouslySetInnerHTML={createMarkup(props.prod.body)}></p>
+        <div className={(props.prod.imageOn=='right') ?
+        'col-lg-5 col-lg-offset-1 col-sm-6 col-sm-pull-6' :
+        'col-lg-5 col-lg-offset-1 col-sm-6' }>
+          <a href={props.prod.target}>
+            <h2 className="section-heading">{props.prod.heading}</h2>
+          </a>
+          <p className="lead" dangerouslySetInnerHTML={{__html: props.prod.body}}></p>
         </div>
       </div>
     </div>
   </div>
 )
-
-const createMarkup = (htmlString) => {
-  return {__html : htmlString}
-}
