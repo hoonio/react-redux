24a4db9e04bd3aca586348fb39b26b5b73c4cd47
Hoonio <hoonio@hoonio.com>
2017-01-21 02:42:16 +0000

    update navbar with new bootstrap4a class

--- app/scripts/components/navbar.js
+++ app/scripts/components/navbar.js
@@ -12,24 +12,22 @@ export default class extends React.Component {
   render() {
     let toggleStyle = (this.state.showMenu) ? {display:'block'}:{display:'none'};
     return (
-      <nav className="navbar navbar-default bg-faded">
-        <div className="container-fluid">
-          <div className="row">
-            <button className="navbar-toggler hidden-sm-up pull-xs-right" type="button" data-toggle="collapse" onClick={ ()=> this.setState({showMenu: !this.state.showMenu})} data-target="#exCollapsingNavbar2">
-              &#9776;
-            </button>
-            <a className="navbar-brand" href="/"><img className="logo" src="https://lh3.googleusercontent.com/-xLJDIl8LBcM/VV0GwIRMygI/AAAAAAACeA4/1IOmES6TSUA/w172-h30-Ic42/hoonio-logo-2014.png"/></a>
-            <div className="navbar-toggleable-xs pull-sm-right" style={toggleStyle} id="exCollapsingNavbar2">
-              <ul role="nav" className="nav navbar-nav">
-                <NavLink to="/" id="nav-home" onlyActiveOnIndex>Home</NavLink>
-                <NavLink to="/profile" onClick={ ()=> this.setState({showMenu: !this.state.showMenu})}>Profile</NavLink>
-                <NavLink to="/portfolio" onClick={ ()=> this.setState({showMenu: !this.state.showMenu})}>Portfolio</NavLink>
-                <NavLink to="/canvas" onClick={ ()=> this.setState({showMenu: !this.state.showMenu})}>Canvas</NavLink>
-                <NavLink to="/blog" onClick={ ()=> this.setState({showMenu: !this.state.showMenu})}>Blog</NavLink>
-                <li className="nav-item"><a className="nav-link" href='/#contact'>Contact</a></li>
-              </ul>
-            </div>
-          </div>
+      <nav className="navbar navbar-toggleable-md navbar-inverse bg-primary fixed-top">
+        <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" onClick={ ()=> this.setState({showMenu: !this.state.showMenu})} data-target="#exCollapsingNavbar2">
+          &#9776;
+        </button>
+        <a className="navbar-brand" href="/">
+          <img className="logo" src="https://lh3.googleusercontent.com/-xLJDIl8LBcM/VV0GwIRMygI/AAAAAAACeA4/1IOmES6TSUA/w172-h30-Ic42/hoonio-logo-2014.png"/>
+        </a>
+        <div className="collapse navbar-collapse" style={toggleStyle} id="exCollapsingNavbar2">
+          <ul role="nav" className="nav navbar-nav">
+            <NavLink to="/" id="nav-home" onlyActiveOnIndex>Home</NavLink>
+            <NavLink to="/profile" onClick={ ()=> this.setState({showMenu: !this.state.showMenu})}>Profile</NavLink>
+            <NavLink to="/portfolio" onClick={ ()=> this.setState({showMenu: !this.state.showMenu})}>Portfolio</NavLink>
+            <NavLink to="/canvas" onClick={ ()=> this.setState({showMenu: !this.state.showMenu})}>Canvas</NavLink>
+            <NavLink to="/blog" onClick={ ()=> this.setState({showMenu: !this.state.showMenu})}>Blog</NavLink>
+            <li className="nav-item"><a className="nav-link" href='/#contact'>Contact</a></li>
+          </ul>
         </div>
       </nav>
     );
