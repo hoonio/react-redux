2b2bdb0d20196a774fe988f48e90530c8dff57c1
Hoonio <hoonio@hoonio.com>
2017-01-21 02:54:03 +0000

    justify navlinks to right on full view

--- app/scripts/components/navbar.js
+++ app/scripts/components/navbar.js
@@ -13,14 +13,14 @@ export default class extends React.Component {
     let toggleStyle = (this.state.showMenu) ? {display:'block'}:{display:'none'};
     return (
       <nav className="navbar navbar-toggleable-md navbar-inverse bg-primary fixed-top">
-        <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" onClick={ ()=> this.setState({showMenu: !this.state.showMenu})} data-target="#exCollapsingNavbar2">
+        <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" onClick={ ()=> this.setState({showMenu: !this.state.showMenu})}>
           &#9776;
         </button>
         <a className="navbar-brand" href="/">
           <img className="logo" src="https://lh3.googleusercontent.com/-xLJDIl8LBcM/VV0GwIRMygI/AAAAAAACeA4/1IOmES6TSUA/w172-h30-Ic42/hoonio-logo-2014.png"/>
         </a>
-        <div className="collapse navbar-collapse" style={toggleStyle} id="exCollapsingNavbar2">
-          <ul role="nav" className="nav navbar-nav">
+        <div className="collapse navbar-collapse justify-content-end" style={toggleStyle}>
+          <ul className="nav navbar-nav">
             <NavLink to="/" id="nav-home" onlyActiveOnIndex>Home</NavLink>
             <NavLink to="/profile" onClick={ ()=> this.setState({showMenu: !this.state.showMenu})}>Profile</NavLink>
             <NavLink to="/portfolio" onClick={ ()=> this.setState({showMenu: !this.state.showMenu})}>Portfolio</NavLink>
