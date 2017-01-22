22173d94785f5dabcefbbb3a5ce729780bb3cea8
Hoonio <hoonio@hoonio.com>
2017-01-22 03:41:01 +0000

    re-insert html copy sequence to build process

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -35,7 +35,8 @@ export default {
       {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file"},
       {test: /\.(woff|woff2)$/, loader: "url?prefix=font/&limit=5000"},
       {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream"},
-      {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml"}
+      {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml"},
+      {test: /\.png$/, loader: 'url?mimetype=image/png'}
     ]
   },
   sassLoader: {
