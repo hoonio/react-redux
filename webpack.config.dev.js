d073841c1dd141e62cb4b62622a1eb115c1d494e
Hoonio <hoonio@hoonio.com>
2017-01-26 02:43:43 +0000

    fix lint on webpack configs

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -9,37 +9,37 @@ export default {
   entry: [
     'eventsource-polyfill',
     'webpack-hot-middleware/client?reload=true',
-    './app/app'
+    './app/app',
   ],
   target: 'web',
   output: {
     path: __dirname + '/dist', // Note: Physical files are only output by the production build task `npm run build`.
     publicPath: '/',
-    filename: 'app.js'
+    filename: 'app.js',
   },
   devServer: {
-    contentBase: './app'
+    contentBase: './app',
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin()
+    new webpack.NoErrorsPlugin(),
   ],
   module: {
     loaders: [
-      {test: /\.js$/,
+      { test: /\.js$/,
         include: path.join(__dirname, 'app'),
         loader: 'babel',
-        query: { presets: ['es2015', 'react', 'stage-2'] }
+        query: { presets: ['es2015', 'react', 'stage-2'] },
       },
-      {test: /\.scss$/, loaders: ["style-loader", "css-loader", "sass-loader"]},
-      {test: /(\.css)$/, loader: ExtractTextPlugin.extract("css?sourceMap")},
-      {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'},
-      {test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000'},
-      {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'},
-      {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'}
-    ]
+      { test: /\.scss$/, loaders: ['style-loader', 'css-loader', 'sass-loader'] },
+      { test: /(\.css)$/, loader: ExtractTextPlugin.extract('css?sourceMap') },
+      { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file' },
+      { test: /\.(woff|woff2)$/, loader: 'url?prefix=font/&limit=5000' },
+      { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream' },
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml' },
+    ],
   },
   sassLoader: {
-    includePaths: [path.resolve(__dirname, "./app/styles")]
-  }
+    includePaths: [path.resolve(__dirname, './app/styles')],
+  },
 };
