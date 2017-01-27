b91d669902b977af0723db76f437035a9103836c
Hoonio <hoonio@hoonio.com>
2017-01-27 02:49:21 +0000

    set dynamic height for the map element

--- app/scripts/containers/voyage.js
+++ app/scripts/containers/voyage.js
@@ -7,29 +7,26 @@ export default class Voyage extends React.Component {
     GoogleMapsLoader.KEY = 'AIzaSyC2OigUrGT2Jhb0T7g5wzroASFt5eRhCG4';
     GoogleMapsLoader.load((google) => {
       const mapOptions = {
-        center: {lat: 24.347, lng: 7.2},
+        center: { lat: 0, lng: 0 },
         zoom: 2,
         minZoom: 2,
         maxZoom: 6,
-        mapTypeControl: false
+        mapTypeControl: false,
       };
 
       const map = new google.maps.Map(document.getElementById('maps'), mapOptions);
 
       const mapData = new google.maps.KmlLayer({
-        url: 'http://www.google.com/maps/d/kml?mid=1P_deVSzm6sY1TRBfFdKjFRnTOWo'
+        url: 'http://www.google.com/maps/d/kml?mid=1P_deVSzm6sY1TRBfFdKjFRnTOWo',
       });
 
       mapData.setMap(map);
-    })
+    });
   }
 
   render() {
-
     return (
-      <div style={{
-        height: '600px'
-      }} id="maps">
+      <div style={{ height: (window.window.innerHeight - 40) + 'px' }} id="maps">
         Map goes in here
       </div>
     );
