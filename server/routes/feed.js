6a7530e64e79c75241705443cec8edb8eb3f34e0
Hoonio <hoonio@hoonio.com>
2017-02-19 16:07:40 +0000

    fetch tumblr feed from server side

--- server/routes/feed.js
+++ server/routes/feed.js
@@ -1,19 +1,25 @@
 import fs from 'fs';
 import path from 'path';
 import express from 'express';
-import fetch from 'fetch'
 import bodyParser from 'body-parser';
+
+require('es6-promise').polyfill();
+import fetch from 'isomorphic-fetch';
+
 const router = express.Router();
 
 router.use(bodyParser.json());
 
 router.get('/blog', function(req, res){
   res.setHeader('Content-Type', 'application/json');
-  const blogFeed = JSON.parse(fs.readFileSync(path.join( __dirname, '../data/blogfeed.json')).toString());
-  res.send(blogFeed);
 
-  //   return jsonp('https://api.tumblr.com/v2/blog/blog.hoonio.com/posts/photo?api_key=o5UJwOYSdRtRCzAwTRfkHVuwUWmTKvmzevn31oTaZ854hHU2r6')
-
+  fetch('https://api.tumblr.com/v2/blog/blog.hoonio.com/posts/photo?api_key=o5UJwOYSdRtRCzAwTRfkHVuwUWmTKvmzevn31oTaZ854hHU2r6')
+    .then(resp => resp.json())
+    .then(json => res.send(json))
+    .catch(err => {
+      console.log(err);
+      res.send(JSON.parse(fs.readFileSync(path.join( __dirname, '../data/blogfeed.json'))));
+    });
 })
 
 module.exports = router;
