7e487f351dfc966ed1c5379bb3901f608504392f
Hoonio <hoonio@hoonio.com>
2017-06-08 23:47:52 +0100

    fix brokend agewatch 2013 html cheerio copy

--- tools/buildHtml.js
+++ tools/buildHtml.js
@@ -32,7 +32,12 @@ fs.readFile('app/index.html', 'utf8', (err, markup) => {
 });
 
 fs.readFile('app/helpage.html', 'utf8', (err, markup) => {
-  fs.writeFile('dist/helpage.html', 'utf8', function (err) {
+  if (err) {
+    return console.log(err);
+  }
+
+  const $ = cheerio.load(markup);
+  fs.writeFile('dist/helpage.html', $.html(), 'utf8', (err) => {
     if (err) {
       return console.log(err);
     }
