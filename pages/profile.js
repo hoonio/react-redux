c540094eb1cc448cbf959e5a1842fa82bf799987
Hoonio <hoonio@hoonio.com>
2018-05-24 18:20:19 +0200

    resolve iframe issue viewpoint height for profile page

---
+++ pages/profile.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default (props) => (
+  <div id="profile" className="container-fluid">
+    <iframe src="https://docs.google.com/document/d/1uQQfy_U0ekb0lo6SjKb6-j1MLTFxwVNR2Ep4H9DcPDI/pub?embedded=true" width="100%"></iframe>
+  </div>
+);
+
+// const getHeight = () => {
+//   let ifrHeight = window.innerHeight - 54;
+//   return ifrHeight;
+// };
