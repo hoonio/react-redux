8199f8ad1656fd1549453b37e90b87185ab03de5
Hoonio <hoonio@hoonio.com>
2018-07-19 18:08:22 +0200

    wip: render stock list

--- pages/_document.js
+++ pages/_document.js
@@ -6,7 +6,7 @@ export default class MyDocument extends Document {
     return (
       <html>
         <Head>
-          <title>Hoonio</title>
+          <title>root</title>
           <meta name="description" content="Bridging the gap between ideas and technology" />
           <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
           <meta property="fb:admins" content="514174661" />
--- pages/canvas.js
+++ pages/canvas.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import { connect } from 'react-redux'
+import { DragDropContext } from 'react-dnd';
+
+// import Dragitem from '../components/dragitem';
+// import Chart from '../components/chart';
 
 class Canvas extends React.Component {
   static async getInitialProps({ req, store, isServer, pathname}) {
@@ -8,20 +12,41 @@ class Canvas extends React.Component {
     return fetch(baseUrl + '/brainwave/list')
     .then(res => res.json())
     .then(data => ({
-      list: data
+      stockList: data
     }))
   }
 
   render() {
-    console.log(this.props.list)
+    console.log(this.props.stockList)
+    const stockList = this.props.stockList ? (this.props.stockList.map((stock, index) =>
+      <div key={index}>{stock}</div>
+      // <Dragitem key={index}
+      //   onclick={() => this.props.selectStock(stock.replace('WIKI/', ''))}
+      //   name={stock.replace('WIKI/', '')}
+      // />
+    )) : null
+
+    // const chart = (<Chart dataset={this.props.dataset}
+    //   ondrop={() => this.props.getStockData(this.props.selectedStock)}
+    // />);
+    const chart = 'chart'
 
     return (
-      <div className="container" id="portfolio">
-        <div className="card-columns">
-          {this.props.list}
+      <div className="container" id="canvas">
+          <h1>Time Series</h1>
+          <p className="lead">Drag any of the stock symbols below to area on the right</p>
+        <div className="row">
+          <div className="col-sm-3 col-xs-12" id="sidebar-frame">
+            <ul className="list-group">
+              {stockList}
+            </ul>
+          </div>
+          <div className="col-sm-9 col-xs-12" id="chart-frame">
+            {chart}
+          </div>
         </div>
       </div>
-    );
+    )
   }
 }
 
